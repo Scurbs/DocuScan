c870e65f239fd323dd8ce8faaaf2947f91491e93
Janes Pozar <janespozar@hotmail.com>
2025-05-31 12:47:01 +0200

    Add files via upload

---
+++ Processing/PdfToImages.py
@@ -0,0 +1,57 @@
+import fitz  
+from PIL import Image, ImageFilter
+import os
+from multiprocessing import Pool
+import time
+
+
+def process_page(page_number, pdf_path, output_folder, dpi):
+    try:
+        start_time = time.time()
+        document = fitz.open(pdf_path)
+        page = document.load_page(page_number)
+        pix = page.get_pixmap(dpi=dpi, alpha=False)
+        
+        img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        img = img.filter(ImageFilter.SHARPEN)
+
+        image_path = os.path.join(output_folder, f"page{page_number + 1}.jpg")
+        img.save(image_path, dpi=(dpi, dpi), quality=95, optimize=True)
+        img.close()
+        document.close()
+
+        print(f"Page {page_number + 1} processed in {time.time() - start_time:.2f} seconds.")
+    except Exception as e:
+        print(f"Failed to process page {page_number + 1}: {str(e)}")
+
+class PDFToImagesConverter:
+    """
+    A class to convert PDF pages to images.
+    """
+    def __init__(self, pdf_path, output_folder, dpi):
+        self.pdf_path = pdf_path
+        self.output_folder = output_folder
+        self.dpi = dpi
+
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+
+    def convert(self):
+
+        try:
+            start_time = time.time()
+            document = fitz.open(self.pdf_path)
+            num_pages = len(document)
+            document.close()
+
+            print(f"Starting conversion of {num_pages} pages.")
+
+            args = [(page_number, self.pdf_path, self.output_folder, self.dpi) for page_number in range(num_pages)]
+
+            #Multiprocessing for daster conversion to img
+            with Pool() as pool:
+                pool.starmap(process_page, args)
+
+            print(f"PDF conversion completed in {time.time() - start_time:.2f} seconds.")
+        except Exception as e:
+            print(f"Failed to convert PDF: {str(e)}")
---
+++ Processing/settings.py
@@ -0,0 +1,55 @@
+import sys
+import os
+
+if getattr(sys, 'frozen', False):
+   
+    BASE_PATH  = os.path.dirname(sys.executable)
+    
+else:
+    BASE_PATH  = r'C:\Users\janes.pozar\Desktop\FastDocu'
+
+EQUIPMENT_FILE = os.path.join(BASE_PATH,'assets','json','equipment.json')
+TESSERACT_PATH = os.path.join(BASE_PATH , 'Tesseract-OCR', 'tesseract.exe')
+TITLE_SHEET = os.path.join(BASE_PATH,'assets','titlesheet.xlsx')
+CONFIG_OCR = '--psm 6 --oem 3 eng'
+
+VERSION = "2.1"
+
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RESET = '\033[0m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+
+RECOVERY_COLOR = '#fcc135'
+RAUMKLASSE_COLOR = '#3db5f5'
+SCANNING_COLOR = '#75d19d'
+INTEGRAL_COLOR = '#d661fa'
+ERROR_COLOR = '#ff6b4a'
+
+U_METER = '\u03BCm'
+
+BLACK = '#000000'
+WHITE = '#EEEEEE'
+
+TITLE_BAR_HEX_COLORS = {
+    'dark': 0x00000000,
+    'light': 0x00EEEEEE
+}
+
+MAIN_ROWS = 4
+MAIN_COLUMNS = 1
+
+SETTINGS_ROWS = 4
+SETTINGS_COLUMNS = 3
+
+EQUIPMENT_COLUMNS = 3
+EQUIPMENT_ROWS = 2
+
+
+FILETYPES = [("PDF files", "*.pdf"), ("Image files", "*.jpg;*.jpeg;*.png"), ("All files", "*.*")]
+DEFAUL_DPI = 300
+
+EQUIPMENT_START_COLUMN = 27
+
