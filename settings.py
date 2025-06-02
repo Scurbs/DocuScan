b5ab3e92e1c9985027047d29078ff2a6c4984bb9
Scurbs <janespozar@hotmail.com>
2025-06-02 17:14:38 +0200

    added sys path

--- settings.py
+++ settings.py
@@ -6,7 +6,12 @@ if getattr(sys, 'frozen', False):
     BASE_PATH  = os.path.dirname(sys.executable)
     
 else:
-    BASE_PATH  = r"E:\Workspace\FastDocu\FastDocu"
+    BASE_PATH  = r""
+TESSERACT_PATH = os.path.join(BASE_PATH , 'Tesseract-OCR', 'tesseract.exe')
+
+CONFIG_OCR = '--psm 6 --oem 3 eng'
+
+VERSION = "2.1"
 
 RED = '\033[91m'
 GREEN = '\033[92m'
@@ -14,6 +19,15 @@ YELLOW = '\033[93m'
 RESET = '\033[0m'
 BLUE = '\033[34m'
 MAGENTA = '\033[35m'
+
+RECOVERY_COLOR = '#fcc135'
+RAUMKLASSE_COLOR = '#3db5f5'
+SCANNING_COLOR = '#75d19d'
+INTEGRAL_COLOR = '#d661fa'
+ERROR_COLOR = '#ff6b4a'
+
+U_METER = '\u03BCm'
+
 BLACK = '#000000'
 WHITE = '#EEEEEE'
 
@@ -35,4 +49,5 @@ EQUIPMENT_ROWS = 2
 FILETYPES = [("PDF files", "*.pdf"), ("Image files", "*.jpg;*.jpeg;*.png"), ("All files", "*.*")]
 DEFAUL_DPI = 300
 
+EQUIPMENT_START_COLUMN = 27
 
